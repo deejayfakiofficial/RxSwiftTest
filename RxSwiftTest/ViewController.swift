917325133f79ad263389e5900343a2e1d8016070
deejayfakiofficial <71965798+deejayfakiofficial@users.noreply.github.com>
2021-01-20 16:18:39 +0400

    add outlets

--- RxSwiftTest/ViewController.swift
+++ RxSwiftTest/ViewController.swift
@@ -7,14 +7,23 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var btn: UIButton!
+    @IBOutlet weak var lbl: UILabel!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textField.rx
+            .controlEvent(.editingChanged)
+            .withLatestFrom(textField.rx.text.orEmpty)
+            .subscribe(onNext: {text in
+                self.lbl.text = text
+            }).disposed(by: disposeBag)
     }
-
-
 }
-
